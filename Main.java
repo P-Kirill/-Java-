f5823bf3d52ec8d4ced5808ca8d52cf0fcce1625
P-Kirill <classic.clash.234@gmail.com>
2026-03-27 20:02:29 +0300

    Задание 2
    Домашнее задание к занятию «Введение в программирование на Java»
    Задание 2

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+ public static void main(String[] args) {
+     int income = 44000;
+     int spending = 30000;
+     int balance = income - spending;
+     System.out.println("Остаток на счету: " + balance + " (руб)");
+ }
+}
